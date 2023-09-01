dddf06cabeb87cf5fcbcde7e1ad43f737bad1ee4
Andrii <andrii.tykhoniuk@ccgames.io>
2023-09-01 18:52:35 +0200

    updated search params

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 import { CarProps } from "@/types"
 import { CustomButton } from "."
-import { calculateCarRent } from "@/utils"
+import { calculateCarRent, generateCarImageUrl } from "@/utils"
 import CarDetails from "./CarDetails"
 // import CustomButton from "./CustomButton"
 
@@ -38,7 +38,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
         <div className="relative w-full h-40 my-3 object-contain">
             <Image 
-                src="/hero.png"
+                src={generateCarImageUrl(car)}
                 alt="car model"
                 fill
                 priority
